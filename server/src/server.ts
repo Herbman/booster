7c2bd27f6afc6ed36c098ebae9f9bada7344d3d2
Brnuo Nery <bruno@prumotecnologia.com.br>
2022-06-17 17:57:39 -0300

    Database set

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,32 +1,9 @@
-import express, { json } from "express";
+import express from "express";
+import routes from './routes';
 
 const app = express();
 
-const users = [
-  'Joe',
-  'Max',
-  'Robb'
-];
-
-app.get('/users', (request, response) => {
-  console.log("Listagem de usuários");
-  return response.json(users);
-});
-
-app.get('/users/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const user = users[id];
-  return response.json(user);
-});
-
-app.post('/users', (request, response) => {
-  const user = {
-    name: 'Ricky',
-    email: 'ricky@teste.com'
-  };
-
-  console.log('Usuário criado');
-  return response.json(user);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333); 
